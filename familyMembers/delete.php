2c7783722e730a1d6a453c3585f2e5e46ae723b5
Anh Thy Vu <anhthyvu26@gmail.com>
2025-04-01 20:53:45 -0400

    General template for delete feature

---
+++ familyMembers/delete.php
@@ -0,0 +1,13 @@
+<?php
+    $page_title = "Family Members";
+    require_once '../database.php';
+
+    //Get the ID of selected family member
+    $personID = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+    //Check if the family member exists
+
+    //Check if the family member is an alternative family member
+
+    //Delete the tuple
+?>
