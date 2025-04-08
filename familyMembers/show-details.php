a8cdfe632405bed9edaf43774b3c45117682a59f
yani1583 <yanizahouani@gmail.com>
2025-04-07 21:51:32 -0400

    queries 7 & 8, added show details for family members and location

--- familyMembers/show-details.php
+++ familyMembers/show-details.php
@@ -1,3 +1,81 @@
+<?php
+    $page_title = "Query 8";
+    require_once '../database.php';
+
+    
+    $Q8Id = isset($_GET['id']) ? $_GET['id'] : 0;
+   
+    $query = "
+       
+
+SELECT
+    secondary_person.FirstName AS SecondaryFirstName,
+    secondary_person.LastName AS SecondaryLastName,
+    secondary_person.PhoneNumber AS SecondaryPhoneNumber,
+    location.Name AS LocationName,
+    cm.CMN AS ClubMembershipNumber,
+    person.FirstName AS ClubMemberFirstName,
+    person.LastName AS ClubMemberLastName,
+    person.DateOfBirth,
+    person.SSN,
+    person.MedicareNumber,
+    person.PhoneNumber AS ClubMemberPhoneNumber,
+    person.Address,
+    person.City,
+    person.Province,
+    person.PostalCode,
+    cm.Relationship AS RelationshipToSecondaryFamilyMember
+
+FROM FamilyMember AS fm
+LEFT JOIN FamilyMember AS secondary_fm
+    ON fm.AlternativeFamilyID = secondary_fm.PersonID
+LEFT JOIN Person AS secondary_person
+    ON secondary_fm.PersonID = secondary_person.PersonID
+JOIN ClubMember AS cm
+    ON cm.PrimaryFamilyID = fm.PersonID
+JOIN Person AS person
+    ON cm.PersonID = person.PersonID
+JOIN Location AS location
+    ON cm.LocationID = location.LocationID
+WHERE fm.PersonID = $Q8Id
+
+    ";
+
+   
+    $result = mysqli_query($conn, $query);
+    
+
+    if (!$result) {
+        die("Query failed: " . mysqli_error($conn));
+    }
+
+   
+    
+$queryName = "
+Select CONCAT(Person.FirstName, ' ', Person.LastName) AS MainName
+From Person
+Where Person.PersonID = $Q8Id
+";
+    
+
+$resultName = mysqli_query($conn, $queryName);
+
+if (!$resultName) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+
+$rowName = mysqli_fetch_assoc($resultName);
+$mainName = $rowName['MainName'];
+?>
+    
+
+
+
+?>
+
+
+
 <head>
     <title><?= $page_title ?></title>
     <link rel="stylesheet" type="text/css" href="../css/navbar.css">
@@ -39,7 +117,7 @@
 
     <!-- Main Section -->
     <main>
-        <h1 style="margin: 2rem 1rem;">More Detail On "Person Name"</h1>
+    <h1 style="margin: 2rem 1rem;">More Detail On <?= htmlspecialchars($mainName) ?></h1>
 
         <!-- Location Details -->
         <div class="list-container">
@@ -52,14 +130,27 @@
                         <th>City</th>
                         <th>Province</th>
                         <th>Postal Code</th>
-                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Displayed dynamically -->
+                <?php
+            // Iterate over the result set for Locations
+            mysqli_data_seek($result, 0); // Reset the pointer to the beginning
+            while ($row = mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td><?= htmlspecialchars($row['LocationName']) ?></td>
+                    <td><?= htmlspecialchars($row['Address']) ?></td>
+                    <td><?= htmlspecialchars($row['City']) ?></td>
+                    <td><?= htmlspecialchars($row['Province']) ?></td>
+                    <td><?= htmlspecialchars($row['PostalCode']) ?></td>
+                </tr>
+            <?php endwhile; ?>
                 </tbody>
             </table>
         </div>
+
+
+
 
         <!-- Secondary Family Member Details -->
         <div class="list-container">
@@ -70,11 +161,20 @@
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Phone Number</th>
-                        <th>Actions</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Displayed dynamically -->
+                <?php
+            // Iterate over the result set for Secondary Family Member
+            mysqli_data_seek($result, 0); // Reset the pointer to the beginning
+            while ($row = mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td><?= htmlspecialchars($row['SecondaryFirstName']) ?></td>
+                    <td><?= htmlspecialchars($row['SecondaryLastName']) ?></td>
+                    <td><?= htmlspecialchars($row['SecondaryPhoneNumber']) ?></td>
+                </tr>
+            <?php endwhile; ?>
                 </tbody>
             </table>
         </div>
@@ -97,11 +197,29 @@
                         <th>Province</th>
                         <th>Postal Code</th>
                         <th>Relationship</th>
-                        <th>Actions</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Displayed dynamically -->
+                <?php
+            // Iterate over the result set for Club Members
+            mysqli_data_seek($result, 0); // Reset the pointer to the beginning
+            while ($row = mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td><?= htmlspecialchars($row['ClubMembershipNumber']) ?></td>
+                    <td><?= htmlspecialchars($row['ClubMemberFirstName']) ?></td>
+                    <td><?= htmlspecialchars($row['ClubMemberLastName']) ?></td>
+                    <td><?= htmlspecialchars($row['DateOfBirth']) ?></td>
+                    <td><?= htmlspecialchars($row['SSN']) ?></td>
+                    <td><?= htmlspecialchars($row['MedicareNumber']) ?></td>
+                    <td><?= htmlspecialchars($row['ClubMemberPhoneNumber']) ?></td>
+                    <td><?= htmlspecialchars($row['Address']) ?></td>
+                    <td><?= htmlspecialchars($row['City']) ?></td>
+                    <td><?= htmlspecialchars($row['Province']) ?></td>
+                    <td><?= htmlspecialchars($row['PostalCode']) ?></td>
+                    <td><?= htmlspecialchars($row['RelationshipToSecondaryFamilyMember']) ?></td>
+                </tr>
+            <?php endwhile; ?>
                 </tbody>
             </table>
         </div>
